8ca21618a49fc42357db6cf26c9fdff5516121ae
Luka Raznatovic <lukaraznatovic@icloud.com>
2022-12-07 14:41:15 +0100

    git gi

--- src/pages/CandidatesPage/CandidatesPage.jsx
+++ src/pages/CandidatesPage/CandidatesPage.jsx
@@ -1,44 +1,44 @@
 
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import Candidate from '../../components/Candidate/Candidate'
 import Footer from '../../components/Footer/Footer'
 import Header from '../../components/Header/Header'
 import Search from '../../components/Search/Search'
 import "./candidatesPage.scss"
 import { dataContext } from '../../context'
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 const CandidatesPage = () => {
-    const {candidates} = useContext(dataContext)
+    const { candidates } = useContext(dataContext)
     return (
-        <>
-            <div className="candidatesPage">
-                CandidatesPage
-                <Header />
-                <div>
-                    <h2>Candidates</h2>
-                    <Search />
-                </div>
-                {candidates?.map((e) => <Link to={`/single-candidates-page/${e.id}`}><Candidate data={e}/></Link>)}
-                <Footer/>
+
+        <div className="candidatesPage">
+            CandidatesPage
+            <Header />
+            <div>
+                <h2>Candidates</h2>
+                <Search />
             </div>
-        </>
-    )
-}
+            {candidates?.map((e) => <Link to={`/single-candidates-page/${e.id}`}><Candidate data={e} /></Link>)}
+            <Footer />
 
 
-        <div id="candidate">
-          <h2>Candidates</h2>
-          <Search />
-        </div>  
+
+
+
+
+            <div id="candidate">
+                <h2>Candidates</h2>
+                <Search />
+            </div>
             <Candidate />
             <Candidate />
             <Candidate />
             <Candidate />
             <Candidate />
-        <Footer />
-      </div>
-    </>
-  );
+            <Footer />
+        </div>
+
+    );
 };
 
 export default CandidatesPage;
